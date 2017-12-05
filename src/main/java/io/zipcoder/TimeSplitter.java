91b60ad91591c9dabf68853646a1ad7a9c23cf42
Leon <leon@Leons-MacBook-Pro.local>
2017-12-04 20:29:39 -0500

    updated TimeSplitter

--- src/main/java/io/zipcoder/TimeSplitter.java
+++ src/main/java/io/zipcoder/TimeSplitter.java
@@ -9,17 +9,17 @@ public class TimeSplitter {
     private final Time time;
 
     public TimeSplitter(String input) {
-        if(!Pattern.matches("\\d{1,2}:[0-6]\\d[aA|pP][mM]",input)) {
+        if (!Pattern.matches("\\d{1,2}:[0-6]\\d[aA|pP][mM]", input)) {
             throw new IllegalArgumentException();
         }
-        int hourStart = 0, hourEnd = input.length() == 7 ? 2 : 1;
-        int minuteStart = hourEnd+1, minuteEnd = minuteStart+2;
-        int meridianStart = minuteEnd, meridianEnd = meridianStart+2;
-            this.time = new Time( // 09:00am
-                    input.substring(hourStart, hourEnd),
-                    input.substring(minuteStart, minuteEnd),
-                    input.substring(meridianStart, meridianEnd));
-
+        int
+                hourStart = 0,             hourEnd = input.length() == 7 ? 2 : 1,
+                minuteStart = hourEnd + 1, minuteEnd = minuteStart + 2,
+                meridianStart = minuteEnd, meridianEnd = meridianStart + 2;
+        this.time = new Time( // 09:00am
+                input.substring(hourStart, hourEnd),
+                input.substring(minuteStart, minuteEnd),
+                input.substring(meridianStart, meridianEnd));
     }
 
     public Time getTime() {
